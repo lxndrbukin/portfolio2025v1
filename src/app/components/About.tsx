065627caf478aed0f0cbfe7cba35de1994c08ebe
Alexander Bukin <lxndrbukin@proton.me>
2025-08-10 10:19:10 +0100

    Add social links to About component under profile picture

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,4 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+const socials = [
+  { name: "GitHub", icon: faGithub, url: "" },
+  { name: "LinkedIn", icon: faLinkedin, url: "" },
+  { name: "Instagram", icon: faInstagram, url: "" },
+];
+
 export default function About() {
+  const renderIcons = () => {
+    return socials.map((social) => {
+      return (
+        <a
+          key={social.name}
+          className="dark:hover:text-sky-500/50"
+          target="_blank"
+          href={social.url}
+        >
+          <FontAwesomeIcon
+            icon={social.icon}
+            style={{ width: "2rem", height: "2rem" }}
+          />
+        </a>
+      );
+    });
+  };
+
   return (
     <section className="grid grid-cols-(--about-grid-cols) pt-[140px]">
       <div>
@@ -13,7 +44,7 @@ export default function About() {
           the world of Web Development.
         </p>
       </div>
-      <div className="grid">
+      <div className="grid gap-2">
         <div className="dark:bg-sky-500/50 h-[270px] w-[270px] rounded-xl relative overflow-hidden justify-self-end">
           <img
             src="https://www.lxndrbukin.com/static/media/hellothere.3c958dea26d332d7d0d7.png"
@@ -21,6 +52,7 @@ export default function About() {
             className="absolute bottom-[-19px] left-[7px]"
           />
         </div>
+        <div className="flex gap-3 justify-self-center">{renderIcons()}</div>
       </div>
     </section>
   );
